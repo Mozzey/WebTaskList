f084ecab491ec02cf22b3d24a81f9f4eeacafbad
Mozzey <mbcacciano@gmail.com>
2018-08-23 21:03:12 -0400

    upon sign up/log in user is currently directed to a list of all tasks - needs to be user specific tasks

---
+++ WebTaskList/WebTaskList/Controllers/UserTasksController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebTaskList.Data;
+using WebTaskList.Domain.Models;
+
+namespace WebTaskList.Controllers
+{
+    public class UserTasksController : Controller
+    {
+        private WebTaskListContext db = new WebTaskListContext();
+
+        // GET: UserTasks
+        public ActionResult Index(User user)
+        {
+            ViewBag.Name = user.Email;
+            var userTasks = db.UserTasks.Include(u => u.User);
+            return View(userTasks.ToList());
+        }
+
+        // GET: UserTasks/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserTask userTask = db.UserTasks.Find(id);
+            if (userTask == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userTask);
+        }
+
+        // GET: UserTasks/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email");
+            return View();
+        }
+
+        // POST: UserTasks/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Description,DueDate,Complete,UserId")] UserTask userTask)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserTasks.Add(userTask);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", userTask.UserId);
+            return View(userTask);
+        }
+
+        // GET: UserTasks/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserTask userTask = db.UserTasks.Find(id);
+            if (userTask == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", userTask.UserId);
+            return View(userTask);
+        }
+
+        // POST: UserTasks/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Description,DueDate,Complete,UserId")] UserTask userTask)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userTask).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", userTask.UserId);
+            return View(userTask);
+        }
+
+        // GET: UserTasks/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserTask userTask = db.UserTasks.Find(id);
+            if (userTask == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userTask);
+        }
+
+        // POST: UserTasks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserTask userTask = db.UserTasks.Find(id);
+            db.UserTasks.Remove(userTask);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
