c494fdd581dd516ef335ce13a09aa8a454fdcc0a
Mozzey <mbcacciano@gmail.com>
2018-08-23 20:51:25 -0400

    sign up page is now default route

--- WebTaskList/WebTaskList/App_Start/RouteConfig.cs
+++ WebTaskList/WebTaskList/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebTaskList
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Users", action = "Create", id = UrlParameter.Optional }
             );
         }
     }
