8b2fc031d7d38b06ae5737a70a8f2f949f5f1ea2
Mozzey <mbcacciano@gmail.com>
2018-08-26 21:48:12 -0400

    touched up styling and routing

--- WebTaskList/WebTaskList/App_Start/RouteConfig.cs
+++ WebTaskList/WebTaskList/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebTaskList
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Users", action = "Create", id = UrlParameter.Optional }
+                defaults: new { controller = "Users", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
